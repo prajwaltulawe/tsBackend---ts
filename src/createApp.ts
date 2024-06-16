7e1c8dded48eef7d16580a95579aedc032d243b7
Prajwal Tulawe <prajwaltulawe@gmail.com>
2024-06-16 21:12:59 +0530

    cookie n session

--- src/createApp.ts
+++ src/createApp.ts
@@ -1,14 +1,36 @@
 import express, { NextFunction, request, Request, response, Response} from "express";
 import usersRouter from './routes/users'
 import cookieParser from "cookie-parser";
+import session from "express-session";
+import { mockUsers } from "./mockdb/constants";
 
 export function createApp() {
     const app = express();
     app.use(cookieParser("secret key"))
-    
+    app.use(session({
+        secret: "qwert",
+        saveUninitialized: false,
+        resave: false,
+        cookie:{
+            maxAge: 60000*60
+        }
+    }))
     app.get('/', (request, response) =>{
+        console.log(request.session)
+        console.log(request.session.id)
         response.cookie("hello", "first cookie", {maxAge: 6000, signed: true})
         response.send("hii")
+    })
+
+    app.post("/", (req, res)=>{
+        const {body: {username, password }} = req;
+        const findUser = mockUsers.find((user)=>{
+            user.username == username
+        })
+        if (!findUser || findUser.password !== password) {
+            return res.status(401).send("not found")
+        }
+        req.session.user = findUser        
     })
 
     app.use('/users', usersRouter)
---
+++ src/mockdb/constants.ts
@@ -0,0 +1,5 @@
+export const mockUsers = [
+    {id: 1, username: "asdf", password: "123"},
+    {id: 2, username: "asdf", password: "456"},
+    {id: 3, username: "asdf", password: "789"},
+]
